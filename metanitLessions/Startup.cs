e51f8f181348d8473ccfcbd09c71a4e997b793bc
Самат Гафиятуллин <stalkersamat@mail.ru>
2021-12-10 22:09:00 +0300

    Просмотр сервисов

--- metanitLessions/Startup.cs
+++ metanitLessions/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -14,26 +15,36 @@ namespace metanitLessions
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IServiceCollection _services;
         public void ConfigureServices(IServiceCollection services)
         {
+            _services = services;
+            var servs = services.ToList();
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.Run(async context =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
+                var sb = new StringBuilder();
+                sb.Append("<h1>Все сервисы<h1>");
+                sb.Append("<table>");
+                sb.Append("<tr><th>Тип</th><th>LifeTime</th><th>Реализация</th></tr>");
+                foreach (var svc in _services)
                 {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                    sb.Append("<tr>");
+                    sb.Append($"<td>{svc.ServiceType.FullName}</td>");
+                    sb.Append($"<td>{svc.Lifetime}</td>");
+                    sb.Append($"<td>{svc.ImplementationType?.FullName}</td>");
+                    sb.Append("</tr>");
+
+                }
+                sb.Append("</table>");
+                context.Response.ContentType = "text/html;charset=utf8";
+                await context.Response.WriteAsync(sb.ToString());
             });
         }
     }
